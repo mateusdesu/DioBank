fda68576b075a1d95eed431be72e9751cee2dc16
Mateus <mateuslopes1717@gmail.com>
2023-06-14 00:09:30 -0300

    jest error fixed

--- src/services/welcome.test.tsx
+++ src/services/welcome.test.tsx
@@ -1,16 +1,16 @@
 import { welcome } from "./welcome";
-import { vi } from "vitest";
 
-// describe("Mensagem de boas vindas", () => {
-//   const mockAlert = vi.fn();
-//   window.alert = mockAlert;
 
-//   it("should return a welcome message", () => {
-//     welcome();
-//     expect(mockAlert).toHaveBeenCalledWith("Boas Vindas!");
-//   });
-// });
+describe("Mensagem de boas vindas", () => {
+  const mockAlert = jest.fn();
+  window.alert = mockAlert;
 
-test("funfa", () => {
-  expect(1 + 1).toBe(2);
-})
+  it("should return a welcome message", () => {
+    welcome();
+    expect(mockAlert).toHaveBeenCalledWith("Boas Vindas!");
+  });
+});
+
+// test("funfa", () => {
+//   expect(1 + 1).toBe(2);
+// })
